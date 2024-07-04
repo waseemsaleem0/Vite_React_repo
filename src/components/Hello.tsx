30062fbfac68e0acb186c220f56d98eab85287ec
Waseem <waseembt10029@hotmail.com>
2024-07-04 14:13:22 +0500

    tailwindcss integrate

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,12 +1,12 @@
 
 import React from 'react';
-
+import '../index.css'
 interface HelloProps {
   name: string;
 }
 
 const Hello: React.FC<HelloProps> = (props) => {
-  return <h1>Hello, {props.name}!</h1>;
+  return <h1 className="text-7xl text-blue-400 text-center underline ">Hello, {props.name}!</h1>;
 };
 
 export default Hello;
